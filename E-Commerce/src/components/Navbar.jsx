271cafb1c0ed8b17316a4ca24595a9e02b7a2882
micaelgoitea <micael.goitea@gmail.com>
2024-04-17 20:38:58 -0300

    PreEntrega1 Goitea

--- E-Commerce/src/components/Navbar.jsx
+++ E-Commerce/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Digital Emporium</a>
+    <a className="navbar-brand" href="#"><img width={100} src="img/logo.png" alt="" /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
