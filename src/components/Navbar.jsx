e42cbd7bceda60b21d0ba8b852fe62b7bac65d49
micaelgoitea <micael.goitea@gmail.com>
2024-05-29 22:22:13 -0300

    Corrigiendo errores de imagenes locales

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import CartWidget from "../components/CartWidget";
 import { NavLink } from 'react-router-dom';
+import imglogo from "../../img/logo.png";
 
 const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <NavLink to="/" className="navbar-brand"><img width={100} src="img/logo.png" alt="Logo de la Marca" /></NavLink>
+          <NavLink to="/" className="navbar-brand"><img width={100} src= {imglogo} alt="Logo de la Marca" /></NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
