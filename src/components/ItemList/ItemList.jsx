2d4794eae4623516c81ba0a925e5735c80f3754b
micaelgoitea <micael.goitea@gmail.com>
2024-05-30 20:56:35 -0300

    Ultimos detalle para la entrega Final

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -3,7 +3,7 @@ import './ItemList.css';
 
 const ItemList = ({ items }) => {
   return (
-      <div className="item-list">
+      <div className="item-list pt-4 mt-4">
         {items.map(item => (
            <Item key={item.id} item={item} />
         ))}
