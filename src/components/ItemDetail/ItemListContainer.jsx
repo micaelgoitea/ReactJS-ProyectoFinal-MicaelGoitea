4e50cc0cb5763ff974a843768afb2a2f7fe0feed
micaelgoitea <micael.goitea@gmail.com>
2024-05-30 21:41:28 -0300

    Agregamos un Pequeño Footer para entrega Final

--- src/components/ItemDetail/ItemListContainer.jsx
+++ src/components/ItemDetail/ItemListContainer.jsx
@@ -5,50 +5,51 @@ import Loading from "../Loading";
 import { useParams } from "react-router-dom";
 import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
 import img_bienvenidos from "../../Assets/bienvenidos.png";
+import logoEmpresa from "../../../img/favicon.png";
 
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
-  const {categoryId} = useParams();
+  const { categoryId } = useParams();
 
-useEffect(() => {
-  const db = getFirestore();
-  const itemsCollection = collection(db, "items");
-  const resultQuery = categoryId ? query(itemsCollection, where("category", "==", categoryId)) : itemsCollection;
-  getDocs(resultQuery).then(snapShot => {
-    if (snapShot.size > 0) {
-      setItems(snapShot.docs.map(item => ({ id: item.id, ...item.data() })));
-      setLoading(false);
-    } else {
-      console.log("No existen Documentos!");
-      setItems([]);
-    }
-  });
-}, [categoryId]);
+  useEffect(() => {
+    const db = getFirestore();
+    const itemsCollection = collection(db, "items");
+    const resultQuery = categoryId ? query(itemsCollection, where("category", "==", categoryId)) : itemsCollection;
+    getDocs(resultQuery).then(snapShot => {
+      if (snapShot.size > 0) {
+        setItems(snapShot.docs.map(item => ({ id: item.id, ...item.data() })));
+        setLoading(false);
+      } else {
+        console.log("No existen Documentos!");
+        setItems([]);
+      }
+    });
+  }, [categoryId]);
 
-// Carga de productos en FireBase
+  // Carga de productos en FireBase
 
-/*   useEffect(() => {
-  const baseDeDatos = getFirestore();
-  const itemsCollection = collection(baseDeDatos, "items");
+  /*   useEffect(() => {
+    const baseDeDatos = getFirestore();
+    const itemsCollection = collection(baseDeDatos, "items");
+    
+    arrayProductos.forEach (producto => {
+      addDoc(itemsCollection, producto)
+    })
   
-  arrayProductos.forEach (producto => {
-    addDoc(itemsCollection, producto)
-  })
+    console.log ("Productos cargados en la base de Datos")
+  
+  }, []); */
 
-  console.log ("Productos cargados en la base de Datos")
+  return (
+    <div className="item-list-container">
+      <img src={img_bienvenidos} className="img-fluid" alt="Portada de Bienvenida"></img>
+      {
+        loading ? <Loading /> : <ItemList items={items}  />
 
-}, []); */
-
-return (
-  <div className="item-list-container">
-    <img src={img_bienvenidos} className="img-fluid" alt="Portada de Bienvenida"></img>
-    {
-      loading ? <Loading /> : <ItemList items={items} />
-    }
-
-  </div>
-)
+      }
+    </div>
+  )
 };
 
 export default ItemListContainer;
